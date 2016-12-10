3c6f0b630ff3cb5cf393d8e2842b02c19de89771
nagypeter <Nagypetikee@hotmail.com>
2016-12-10 10:52:40 +0100

    Web Stack impro

--- webservice/index.py
+++ webservice/index.py
@@ -3,10 +3,59 @@ app = Flask(__name__)
 import sys
 sys.path.insert(0,"../DL")
 import network
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+import json
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, str):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 model = None
 
 @app.route("/", methods =['GET','POST'])
+@crossdomain(origin='*')
 def push():
     if request.method == 'POST':
         gender =  request.form["gender"]
@@ -15,9 +64,14 @@ def push():
         city =  request.form["city"]
         f,m,a,days = format(gender,age,day)
 
-        print(network.model_predict("../DL/hungarian_city_coor.csv",model,m,f,a,days,city))
-
-        return "POST"
+        data = network.model_predict("../DL/hungarian_city_coor.csv",model,m,f,a,days,city)
+        data = data.transpose()
+        data = data.tolist()
+        for x in range(len(data)):
+            for y in range(len(data[x])):
+                data[x][y] = str(data[x][y])
+        print(data)
+        return json.dumps(data)
     else:
         return "Use the service with HTTP POST method - params: {gender,age,day,city}"
 
--- website/js/scripts.js
+++ website/js/scripts.js
@@ -1,69 +1,85 @@
-$(document).ready( () => {
+$(document).ready(() => {
 
-    var data = [
-        {
-          z: [
-              [0.11182085 ,0.11284743 ,0.1082705  ,0.10926836 ,0.11242672 ,0.1110902
-               ,0.11448877 ,0.11095188 ,0.10883519],
-
-              [0.09827232 ,0.13256048 ,0.0982589  ,0.09235884 ,0.09832077 ,0.11065163
-               ,0.12208363 ,0.13110511 ,0.11638842],
-                [0.10469079,0.11319802 ,0.10676098 ,0.10551472 ,0.10219582 ,0.10892998
-               ,0.1099606  ,0.12309095 ,0.1256582 ],
-                [0.11043172 ,0.10262392 ,0.11172075 ,0.11632952 ,0.1069346  ,0.12202435
-               ,0.11721086 ,0.10725526 ,0.10546898],
-                [0.10829079 ,0.10343078 ,0.09587876 ,0.10375295 ,0.11709768 ,0.11976036
-               ,0.13561907,0.12008412 ,0.0960855 ],
-                [0.10965586 ,0.10908791 ,0.11535865 ,0.10128407 ,0.12425452 ,0.10222045
-               ,0.11640888 ,0.11859152 ,0.10313816],
-                [0.08872823 ,0.09284715 ,0.12910238 ,0.0883479  ,0.09091653 ,0.07584139
-             ,0.13067463 ,0.14151287 ,0.16202889]
-
-            ],
-            colorscale: [
-            ['0.0', 'rgb(64,8,4)'],
-            ['0.111111111111', 'rgb(127,16,8)'],
-            ['0.222222222222', 'rgb(255,40,67)'],
-            ['0.333333333333', 'rgb(191,25,12)'],
-            ['0.444444444444', 'rgb(239,3,14)'],
-            ['0.555555555556', 'rgb(170,170,170)'],
-            ['0.666666666667', 'rgb(65,229,49)'],
-            ['0.777777777778', 'rgb(54,191,41)'],
-            ['0.888888888889', 'rgb(36,127,27)'],
-            ['1.0', 'rgb(18,64,14)']
-          ],
-          y: ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
-          x: ["17:00","16:00","15:00","14:00","13:00","12:00","11:00","10:00","9:00"],
-          type: "heatmap",
-          hoverinfo: "z",
-        }
-    ];
-
-    Plotly.newPlot('chart',data);
-
-    var weekdays = ["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"];
+    var weekdays = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
     $('#weekday-text').val(weekdays[new Date().getDay()]);
 
-    $('#gender-dropdown li').on('click',(e) => {
-      var elem = e.target;
-      var label = elem.text;
-      $('#gender-text').val(label);
+    $('#gender-dropdown li').on('click', (e) => {
+        var elem = e.target;
+        var label = elem.text;
+        $('#gender-text').val(label);
     });
 
-    $('#weekday-dropdown li').on('click',(e) => {
-      var elem = e.target;
-      var label = elem.text;
-      $('#weekday-text').val(label);
+    $('#weekday-dropdown li').on('click', (e) => {
+        var elem = e.target;
+        var label = elem.text;
+        $('#weekday-text').val(label);
     });
+
 
     $('#submit').on('click', (e) => {
-         $.ajax({
-           method: 'POST',
-           url: 'http://localhost:5000',
-           data: { gender: $('#gender-text').val(), age: $('#age-text').val(), day: $('#weekday-text').val(), city: $('#city-text').val() }
-         }).done( (result) => {
-            console.log(result);
-         });
+        $.ajax({
+            method: 'POST',
+            url: 'http://localhost:5000',
+            data: {
+                gender: $('#gender-text').val(),
+                age: $('#age-text').val(),
+                day: $('#weekday-text').val(),
+                city: $('#city-text').val()
+            }
+        }).done((result) => {
+            result = JSON.parse(result);
+            console.log(result)
+            var array = [];
+            var second = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+            for (var j = 0; j < 9; j++)
+                array.push(second);
+
+            console.log(array)
+
+            for (var i = 0; i < 9; i++) {
+                var row = []
+                for (var j = 0; j < 7; j++) {
+                    row.push(parseFloat(result[i][j]))
+                }
+                array[i] = row.slice()
+            }
+            console.log(array);
+
+            var data = [{
+                z: array,
+                colorscale: [
+                    // ['0.0', 'rgb(64,8,4)'],
+                    // ['0.111111111111', 'rgb(127,16,8)'],
+                    // ['0.222222222222', 'rgb(255,40,67)'],
+                    // ['0.333333333333', 'rgb(191,25,12)'],
+                    // ['0.444444444444', 'rgb(239,3,14)'],
+                    // ['0.555555555556', 'rgb(170,170,170)'],
+                    // ['0.666666666667', 'rgb(65,229,49)'],
+                    // ['0.777777777778', 'rgb(54,191,41)'],
+                    // ['0.888888888889', 'rgb(36,127,27)'],
+                    // ['1.0', 'rgb(18,64,14)']
+
+                    ['0.0', 'rgb(120,12,13)'],
+                    ['0.111111111111', 'rgb(170,30,40)'],
+                    ['0.222222222222', 'rgb(200,25,25)'],
+                    ['0.333333333333', 'rgb(210,25,20)'],
+                    ['0.444444444444', 'rgb(220,20,25)'],
+                    ['0.555555555556', 'rgb(170,170,170)'],
+                    ['0.666666666667', 'rgb(120,200,120)'],
+                    ['0.777777777778', 'rgb(90,235,80)'],
+                    ['0.888888888889', 'rgb(65,229,49)'],
+                    ['1.0', 'rgb(54,191,41)']
+                ],
+                x: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+                y: ["17:00", "16:00", "15:00", "14:00", "13:00", "12:00", "11:00", "10:00", "9:00"],
+                type: "heatmap",
+                hoverinfo: "z",
+            }];
+
+            Plotly.newPlot('chart', data);
+
+
+        });
     });
 
 });
